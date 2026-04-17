68764fc8cb6f44027aca45506d64139c953cb79e
Shuxian Wang <shuxian.wang@antithesis.com>
2026-04-17 10:39:20 -0400

    Update flake

--- flake.nix
+++ flake.nix
@@ -5,10 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
     flakelight.url = "github:nix-community/flakelight";
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
@@ -16,13 +13,13 @@
     inherit inputs;
     withOverlays = [
       inputs.rust-overlay.overlays.default
-      (final: { inputs', lib, rust-bin, ... }:
+      (final: { lib, rust-bin, ... }:
         let
           # version:
           # "latest" => latest stable
           # "nightly" => latest nightly
           # "1.61.0" => specific stable version
-          craneLib = version: inputs'.crane.lib.overrideToolchain (if version == "nightly" then rust-bin.nightly.latest.default else rust-bin.stable.${version}.default);
+          craneLib = version: (inputs.crane.mkLib final).overrideToolchain (if version == "nightly" then rust-bin.nightly.latest.default else rust-bin.stable.${version}.default);
           commonArgs = {
             src = ./.;
             pname = "antithesis-sdk-rust-workspace";
@@ -63,8 +60,8 @@
 
     packages = rec {
       default = workspace;
-      workspace = { antithesis-sdk-rust }: antithesis-sdk-rust.workspace;
-      doc = { antithesis-sdk-rust }: antithesis-sdk-rust.doc;
+      workspace = pkgs: pkgs.antithesis-sdk-rust.workspace;
+      doc = pkgs: pkgs.antithesis-sdk-rust.doc;
     };
 
     apps = rec {
